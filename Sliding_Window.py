2ac3ba46e53646090afa2bceec38983b6ba8b92a
Ashitosh Golabanvi <84953607+Ashitosh-Golabanvi@users.noreply.github.com>
2024-10-21 00:12:54 +0530

    Update Sliding_Window.py

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -30,3 +30,19 @@ print(Slide(arr, k))
 
 
 # Problem Statement: Given a string, find the length of the longest substring without repeating characters.
+def Long(s):
+  char_set = set()
+  left = 0
+  max_len = 0
+
+  for right in range(len(s)):
+    while s[right] in char_set:
+      char_set.remove(s[left])
+      left += 1
+
+    char_set.add(s[right])
+    max_len = max(max_len, right - left +1)
+  return max_len
+
+s = input("Enter value:")
+print(Long(s))
