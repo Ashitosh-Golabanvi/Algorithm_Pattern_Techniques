d26d68f27fd35977595f8879821b4e992053d10a
Ashitosh Golabanvi <84953607+Ashitosh-Golabanvi@users.noreply.github.com>
2024-10-21 20:04:42 +0530

    Update Two_Pointer.py

--- Two_Pointer.py
+++ Two_Pointer.py
@@ -1,1 +1,25 @@
 # Two Pointer: IS algorithm Technique, which is used to optimize the time and space complexity, which is used to find the perticular sum of two elements in list, and used to reverse the string.
+# Finding a pair with a given sum in a sorted array:
+def Two(arr, target):
+  left, right =0, len(arr)-1
+  while left < right:
+    current_sum = arr[left] + arr[right]
+
+    if current_sum == target:
+      return True
+
+    elif current_sum < target:
+      left += 1
+
+    else:
+      right -= 1
+  return False
+
+a = int(input(":"))
+arr = []
+for i in range(a):
+  b=int(input(":"))
+  arr.append(b)
+target=int(input(":"))
+print(Two(arr,target))
+
