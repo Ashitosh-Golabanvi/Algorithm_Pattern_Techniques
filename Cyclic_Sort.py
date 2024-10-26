01acaee3519909eac173e6e3567c4a334f5b766a
Ashitosh Golabanvi <84953607+Ashitosh-Golabanvi@users.noreply.github.com>
2024-10-26 21:13:29 +0530

    Create Cyclic_Sort.py

---
+++ Cyclic_Sort.py
@@ -0,0 +1,38 @@
+***Cyclic Sort:*** which is basically sorting algorithm.
+* It starts from Initial Element.
+* And Checks the current element is at right place or not.
+* If It's Not in Right Place, then It will swap the current element to currect place.
+* this process repeats until all the elements should place at right place or until end of array elements.
+
+
+** Conditions:**
+* The major Drowback Of This Pattern is the Given elements of inputs should be Naughbor Elements
+
+* Ex: I/P: 3,1,5,4,2
+* Like this Inputs won't work: 5,6,1,8,10
+
+
+def Cycle(arr):
+  i = 0
+
+  while i < len(arr):
+    correct_index = arr[i] - 1
+
+    if arr[i] != arr[correct_index]:
+      arr[i], arr[correct_index] = arr[correct_index], arr[i]
+
+    else:
+      i += 1
+
+
+  return arr
+
+
+
+a = int(input("Enter How many Number of elements you want enter:"))
+arr = []
+for i in range(a):
+  b = int(input("ENter The Values:"))
+  arr.append(b)
+sorted_arr = Cycle(arr)
+print("Sorted_Values:", sorted_arr)
